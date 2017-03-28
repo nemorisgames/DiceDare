7b5b4f8bc62ee697205ca7591ee41b148c630654
nemorisgames <contacto@nemorisgames.com>
2017-03-28 11:19:33 -0300

    ads agregados

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	public void play(){
+		SceneManager.LoadScene ("LevelSelection");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
