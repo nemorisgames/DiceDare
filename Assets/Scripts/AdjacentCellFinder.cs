687816f2b9c270d49203fb97f3ebb6a7b6e4d93d
njaramillo-ng <njaramillo@nemorisgames.com>
2017-08-14 13:14:09 -0300

    deteccion celdas adyacentes + caida en borde

---
+++ Assets/Scripts/AdjacentCellFinder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjacentCellFinder : MonoBehaviour {
+	InGame ingame;
+
+	// Use this for initialization
+	void Start () {
+		ingame = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<InGame>();
+	}
+
+	void OnTriggerStay(Collider c){
+		if (!ingame.rotating && c.name.Substring (0, 4) == "Cell") {
+			Cell cell = c.GetComponent<Cell> ();
+			if (cell.stateCell != Cell.StateCell.Passed)
+				//StartCoroutine(cell.shine (1));
+				Debug.Log(this.name+": "+cell.number);
+		}
+	}
+
+	void OnTriggerExit(Collider c){
+		if (c.name.Substring (0, 4) == "Cell") {
+			Cell cell = c.GetComponent<Cell> ();
+			Debug.Log (this.name + ": " + cell.number);
+		}
+	}
+}
--- Assets/Scripts/InGame.cs
+++ Assets/Scripts/InGame.cs
@@ -50,7 +50,7 @@ public class InGame : MonoBehaviour {
 	public AudioSource bgm_go;
 	public static AudioSource bgm;
 
-	public TutorialVideo tutorialVideo;
+	//public TutorialVideo tutorialVideo;
 
 	//[HideInInspector]
 	public bool pause = false;
@@ -64,6 +64,7 @@ public class InGame : MonoBehaviour {
 
 	public bool testing = false;
 	float diceSize;
+	public Transform adjacentCells;
 
 	// Use this for initialization
 	void Start () {
@@ -219,8 +220,6 @@ public class InGame : MonoBehaviour {
 			//StartCoroutine(tutorialVideo.PlayClip(int.Parse (info [4]) - 1));
 			//Pause ();
 		}
-		else
-			tutorialVideo.gameObject.SetActive(false);
 		
 		string completoNumbers = "";
 		switch (PlayerPrefs.GetString ("scene", "Scene1")) {
@@ -420,7 +419,7 @@ public class InGame : MonoBehaviour {
 		dice.enabled = false;
 		dice.transform.rotation = Quaternion.identity;
 		dice.GetComponent<Animator> ().SetTrigger ("Finished");
-		tutorialVideo.ToggleOff ();
+		//tutorialVideo.ToggleOff ();
 		audio.pitch = 1f;
 		audio.PlayOneShot(audioFinish);
         if (Time.timeSinceLevelLoad - pauseTime < PlayerPrefs.GetFloat("record" + PlayerPrefs.GetString("scene", "Scene1"), float.MaxValue))
@@ -617,5 +616,7 @@ public class InGame : MonoBehaviour {
 				PlayerPrefs.DeleteAll ();
 			}
 		}
+
+		adjacentCells.position = dice.transform.position;
 	}
 }
