88e352265ec0b98474e3e534d9b5443d15cbdf35
njaramillo-ng <njaramillo@nemorisgames.com>
2017-08-22 12:57:18 -0300

    tutorial para portrait

--- Assets/Scripts/TutorialVideo.cs
+++ Assets/Scripts/TutorialVideo.cs
@@ -6,20 +6,44 @@ using UnityEngine.Video;
 public class TutorialVideo : MonoBehaviour {
 	VideoPlayer vp;
 	Animator anim;
+	DeviceOrientation lastOrientation;
 	//public VideoClip [] clips; //0 = suma, 1 = resta, 2 = multi, 3 = div
 
 	// Use this for initialization
 	void Start () {
 		vp = GetComponent<VideoPlayer> ();
 		anim = GetComponentInChildren<Animator> ();
+
+		#if !UNITY_EDITOR
+		lastOrientation = Input.DeviceOrientation;
+		#else
+		//lastOrientation = DeviceOrientation.LandscapeLeft;
+		anim.SetTrigger("Landscape");
+		#endif
+
 		vp.skipOnDrop = false;
 		vp.Prepare ();
 		//Debug.Log (vp.frameRate);
 	}
-	
-	// Update is called once per frame
-	void Update () {
 
+	void Update(){
+		DeviceOrientation currentOrientation = Input.deviceOrientation;
+
+		#if UNITY_EDITOR
+		if(Input.GetKeyDown(KeyCode.M))
+			currentOrientation = DeviceOrientation.LandscapeLeft;
+		if(Input.GetKeyDown(KeyCode.N))
+			currentOrientation = DeviceOrientation.Portrait;
+		#endif
+
+		if (currentOrientation != lastOrientation) {
+			lastOrientation = currentOrientation;
+			if (lastOrientation == DeviceOrientation.Portrait) {
+				anim.SetTrigger ("Portrait");
+			} else if (lastOrientation == DeviceOrientation.LandscapeLeft || lastOrientation == DeviceOrientation.LandscapeRight) {
+				anim.SetTrigger ("Landscape");
+			}
+		}
 	}
 
 	/*public IEnumerator PlayClip(int i){
